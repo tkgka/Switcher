614a42bcceb9fd465ce6678c405c13753f968865
tkgka <ksh17171@gmail.com>
2022-01-07 18:10:36 +0900

    Initial Commit

---
+++ Switcher/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Switcher
+//
+//  Created by 김수환 on 2022/01/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Switcher/SwitcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwitcherApp.swift
+//  Switcher
+//
+//  Created by 김수환 on 2022/01/07.
+//
+
+import SwiftUI
+
+@main
+struct SwitcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
