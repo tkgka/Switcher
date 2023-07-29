e6617fa8a750a574e75cdd25a49797d019db14c9
tkgka <ksh17171@gmail.com>
2023-07-29 23:41:30 +0900

    Implement [Switcher#13] make Key execute when Press same key twice
    - currently all keys execute when it pressed twice

--- Switcher/Controller/KeyInputController.swift
+++ Switcher/Controller/KeyInputController.swift
@@ -10,6 +10,36 @@ import SwiftUI
 struct KeyInputController {
     
     static func handle(event: NSEvent, cgEvent: CGEvent, wrapper: Wrapper, proxy: CGEventTapProxy) -> CGEvent? {
-        cgEvent
+        guard event.type == .keyDown || event.type == .keyUp else {
+            return cgEvent
+        }
+        
+        let result = preventKeyPressByMistake(event: event)
+        return result
     }
+}
+
+
+// MARK: - Prevent Key Press By Mistake
+
+
+private extension KeyInputController {
+    
+    static func preventKeyPressByMistake(event: NSEvent) -> CGEvent? {
+        guard event.keyCode == lastPressedKeyEvent?.keyCode,
+              event.modifierFlags.rawValue == lastPressedKeyEvent?.modifierFlags.rawValue
+        else {
+            if event.type == .keyDown {
+                lastPressedKeyEvent = event
+            }
+            return nil
+        }
+        
+        if event.type == .keyDown {
+            lastPressedKeyEvent = nil
+        }
+        return event.cgEvent
+    }
+    
+    static var lastPressedKeyEvent: NSEvent?
 }
